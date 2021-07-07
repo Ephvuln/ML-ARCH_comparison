3a0f8a7cc4bfb6ac23aacf2c4649240a39b32c0f
Ephvuln <alex.bogatu@yahoo.com>
2021-07-07 15:38:15 +0300

    Fixed traning VGG instead of transfer learning.

--- load_data.py
+++ load_data.py
@@ -5,6 +5,7 @@ image_shape = (224,224)
 input_shape = image_shape+(3,)
 means = [0.485, 0.456, 0.406]
 stds = [0.229, 0.224, 0.225]
+variances = [0.229*0.229, 0.224*0.224, 0.225*0.225]
 
 training_set = tf.keras.preprocessing.image_dataset_from_directory("./Dataset/train/",image_size=image_shape,label_mode='binary',color_mode='rgb')#color_mode='grayscale')
 test_set = tf.keras.preprocessing.image_dataset_from_directory("./Dataset/valid/",image_size=image_shape,label_mode='binary',color_mode='rgb')#color_mode='grayscale')
@@ -13,11 +14,11 @@ test_set = tf.keras.preprocessing.image_dataset_from_directory("./Dataset/valid/
 # https://keras.io/guides/transfer_learning/
 
 model = tf.keras.applications.VGG16(weights='imagenet',input_shape=input_shape,include_top=False)
-
+model.trainable = False
 
 inputs = tf.keras.Input(shape=input_shape)
 x = tf.keras.layers.experimental.preprocessing.Rescaling(scale=1./255)(inputs)
-x = tf.keras.layers.experimental.preprocessing.Normalization(mean=means,variance=stds)(x)
+x = tf.keras.layers.experimental.preprocessing.Normalization(mean=means,variance=variances)(x)
 
 x =  model(x,training=False)
 x = tf.keras.layers.Flatten()(x)
